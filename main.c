05936c07f58aca0a4c2db607d0755ca1953d45ed
umutsahinturk <umut.sahinturk@std.yildiz.edu.tr>
2025-01-12 07:58:14 +0300

    Konvolüsyon işlemi fonksiyonu, ve fonksiyona verilecek parametreleri ayarlayan bölümler eklendi

--- main.c
+++ main.c
@@ -4,7 +4,9 @@
 #define N 4
 #define k 3
 
-int main()
+konvolusyon_islemi(int G[M][N], int F[k][k], int i,  int j, int FT);
+
+int main() 
 {
     int goruntu_matrisi[M][N] =
     {{0, 1, 2, 0},
@@ -17,11 +19,30 @@ int main()
      {2, 3, 1},
      {0, 1, 2}};
 
-    int sonuc_matrisi[M - k + 1][N – k + 1];
+    int sonuc_matrisi[M - k + 1][N - k + 1];
 
+    int filtre_matrisinin_elemanlar_toplami;
 
+    for (int i = 1, i <= k; i++)
+    {
+        for (int j = 1, j <= k; j++)
+        {
+            filtre_matrisinin_elemanlar_toplami += filtre_matrisi[i][j];
+        }
+    }
 
-
+    for (int i = 1; i <= M - k + 1; i++) 
+    {
+        for (int j = 1; j <= N - k + 1; j++) 
+        {
+            sonuc_matrisi[i][j] = konvolusyon_islemi(goruntu_matrisi, filtre_matrisi, i, j, filtre_matrisinin_elemanlar_toplami);
+        }
+    } 
 
     return 0;
+}
+
+konvolusyon_islemi(int G[M][N], int F[k][k], int i,  int j, int FT)
+{
+    
 }
