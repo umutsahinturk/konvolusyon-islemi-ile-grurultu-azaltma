5a1ff2de29427657fd49cb849a9bfee93a236c1a
umutsahinturk <umut.sahinturk@std.yildiz.edu.tr>
2025-01-12 07:11:25 +0300

    main.c dosyası oluşturuldu. gürültü matrisi, filtre matrisi ve sonuç matrisi tanımlandı.

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+#define M 4
+#define N 4
+#define k 3
+
+int main()
+{
+    int goruntu_matrisi[M][N] =
+    {{0, 1, 2, 0},
+     {3, 4, 5, 2},
+     {6, 7, 1, 1},
+     {1, 2, 3, 4}};
+
+    int filtre_matrisi[k][k] =
+    {{0, 1, 0},
+     {2, 3, 1},
+     {0, 1, 2}};
+
+    int sonuc_matrisi[M - k + 1][N – k + 1];
+
+
+
+
+
+    return 0;
+}
